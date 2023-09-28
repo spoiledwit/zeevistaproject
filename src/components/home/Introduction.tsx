3e4fa2817b39a23fef7a1e9acca6715c5cbfa3ec
Ahmad Ashfaq <spoiledwit@gmail.com>
2023-09-28 11:20:05 +0500

    introduction set

--- src/components/home/Introduction.tsx
+++ src/components/home/Introduction.tsx
@@ -13,19 +13,19 @@ const Introduction = () => {
 
                 <Bar w={"w-[280px]"} />
             </h2>
-            <div className="mt-12 flex font-play max-w-screen-lg gap-10 mx-auto text-sm leading-relaxed">
-                <div className="w-1/2 border-r border-yellow-600 pr-5">
+            <div className="mt-12 md:flex-row flex-col flex font-play max-w-screen-lg gap-10 mx-auto text-sm leading-relaxed">
+                <div className="md:w-1/2 md:px-0 px-8 md:border-r border-yellow-600 pr-5">
                     Recognized by governments worldwide,ZeeVista Immigration Advisors specialize in high-quality second citizenship and residency programs. Our international team is founded on a legacy of professionalism and ethical conduct.
                 </div>
-                <div className="w-1/2 pl-5">
+                <div className="md:w-1/2 md:px-0 px-8 md:pl-5">
                     As a government-approved agency, we guide clients through the seamless process of acquiring citizenship by investment, thereby opening doors to global opportunities and an enhanced lifestyle.
                 </div>
             </div>
-            <div className="mt-8 flex font-play max-w-screen-lg gap-10 mx-auto text-sm leading-relaxed">
-                <div className="w-1/2 border-r border-yellow-600 pr-5">
+            <div className="mt-8 flex md:flex-row md:px-0 px-8 flex-col  font-play max-w-screen-lg gap-10 mx-auto text-sm leading-relaxed">
+                <div className="md:w-1/2 md:border-r border-yellow-600 pr-5">
                     Our services extend beyond documentation. We offer comprehensive support that ensures a smooth transition for our clients, from initial consultation to successful application.
                 </div>
-                <div className="w-1/2 pl-5">
+                <div className="md:w-1/2 md:pl-5">
                     Our commitment to excellence is backed by strict adherence to industry standards and regulations, guaranteeing a premium, confidential, and hassle-free experience.
                 </div>
             </div>
