e8b1bb710ab171224477dcb3395a06f12eefedbc
M Talha <132173567+MTalha250@users.noreply.github.com>
2024-09-11 21:08:22 +0500

    update

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,10 +1,20 @@
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import AssessmentForm from "../assessment-form";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+import Input from "../input";
+import Button from "../Button";
 
 const Contact = () => {
   const controls = useAnimation();
-
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const controlsForm = useAnimation();
   const { ref: refForm, inView: inViewForm } = useInView({
@@ -24,6 +34,34 @@ const Contact = () => {
   if (inViewForm) {
     controlsForm.start("visible");
   }
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (!name || !email || !phone || !subject || !message) {
+      toast.error("Please fill all the fields!");
+      return;
+    }
+    setLoading(true);
+    try {
+      await axios.post(`${import.meta.env.VITE_SERVER_URL}/quote`, {
+        name,
+        email,
+        phone,
+        subject,
+        message,
+      });
+      toast.success("Message Submitted successfully!");
+      setPhone("");
+      setEmail("");
+      setMessage("");
+      setName("");
+      setSubject("");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <motion.div
@@ -51,14 +89,50 @@ const Contact = () => {
               animate={{ scale: 1.2 }}
               transition={{ duration: 1 }}
             >
-              Contact Us
+              Get a Quote
             </motion.p>
           </div>
-          <AssessmentForm />
+          <form onSubmit={handleSubmit} className="w-full flex flex-col gap-8">
+            <Input
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              label="Email"
+              value={email}
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              label="Mobile Number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <Input
+              value={subject}
+              label="Subject"
+              onChange={(e) => setSubject(e.target.value)}
+            />
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="border-b-[2px] border-black/30  rounded-none shadow-none py-2 appearance-none text-gray-600 text-sm outline-none focus:border-yellow-600 ring-0 focus:delay-[99999] peer"
+              placeholder="Message"
+            ></textarea>
+            {loading ? (
+              <Button text="Loading..." onClick={() => {}} />
+            ) : (
+              <Button text="Submit" onClick={() => handleSubmit} />
+            )}
+          </form>
         </motion.div>
-        <iframe 
-       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.735670147676!2d55.263824374836155!3d25.178401677723095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69cb98cd9041%3A0xe6be587070e7a89d!2sDAMAC%20Executive%20Bay!5e0!3m2!1sen!2s!4v1696005211967!5m2!1sen!2s"
-       className="md:w-[530px] mt-6 md:mt-0 rounded-xl border hover:border-black" height="530" loading="lazy"></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.735670147676!2d55.263824374836155!3d25.178401677723095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69cb98cd9041%3A0xe6be587070e7a89d!2sDAMAC%20Executive%20Bay!5e0!3m2!1sen!2s!4v1696005211967!5m2!1sen!2s"
+          className="md:w-[530px] mt-6 md:mt-0 rounded-xl border hover:border-black"
+          height="530"
+          loading="lazy"
+        ></iframe>
       </div>
     </motion.div>
   );
--- src/navs/testimonails.tsx
+++ src/navs/testimonails.tsx
@@ -1,9 +1,25 @@
 import Carousel from "../components/carousel/simple";
 import Bar from "../components/Bar";
-import globe from "../../assets/globe.jpg";
 import { AiFillStar } from "react-icons/ai";
 import CountUp from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
+import m1 from "../../assets/people/m1.jpg";
+import m2 from "../../assets/people/m2.jpg";
+import m3 from "../../assets/people/m3.jpg";
+import m4 from "../../assets/people/m4.jpg";
+import m5 from "../../assets/people/m5.jpg";
+import m6 from "../../assets/people/m6.jpg";
+import m7 from "../../assets/people/m7.jpg";
+import m8 from "../../assets/people/m8.jpg";
+import m9 from "../../assets/people/m9.jpg";
+import m10 from "../../assets/people/m10.jpg";
+import f1 from "../../assets/people/f1.jpg";
+import f2 from "../../assets/people/f2.jpg";
+import f3 from "../../assets/people/f3.jpg";
+import f4 from "../../assets/people/f4.jpg";
+import f5 from "../../assets/people/f5.jpg";
+import f6 from "../../assets/people/f6.jpg";
+import f7 from "../../assets/people/f7.jpg";
 
 export default function Testimonials() {
   const testimonials = [
@@ -11,104 +27,103 @@ export default function Testimonials() {
       name: "Ahmed S",
       content:
         "ZeeVista made my dream of immigration to UK from Dubai come true with their professional team who guided me step-by-step. I am grateful for their exceptional service and support throughout this journey.",
-      image: globe,
+      image: m1,
     },
     {
       name: "Maryam K",
       content:
         "ZeeVista was exceptionally helpful when I needed help getting a work permit for Canada from Dubai. Their team was professional and knowledgeable, and I am now happily settled in my new home.",
-      image: globe,
+      image: f1,
     },
     {
       name: "Sania Iqbal",
       content:
         "The professionals at ZeeVista Advisors were remarkable in handling my immigration case. Their knowledge and diligence ensured a smooth process, making my dream of relocating a reality. I am eternally grateful for their exceptional service and support throughout this journey.",
-      image: globe,
+      image: f2,
     },
     {
       name: "Rohit Mehra",
       content:
         "The expertise and professionalism displayed by ZeeVista Advisors were unmatched. They took the time to understand my case and provided tailored solutions, making the visa application a breeze. I highly recommend their services to anyone seeking a hassle-free immigration experience.",
-      image: globe,
+      image: m2,
     },
     {
       name: "Fahad Hussain",
       content:
         "ZeeVista Advisors have been a pillar of support during my visa application. Their thorough understanding of the process and timely guidance made a significant difference. Today, I am thriving in a new country, all thanks to their exemplary services.",
-      image: globe,
+      image: m3,
     },
     {
       name: "Ayesha Rahman",
       content:
         "Entrusting ZeeVista Advisors with my immigration process was the best decision. They showcased exceptional professionalism and attention to detail, ensuring a seamless journey towards achieving my goals. I can't thank them enough for their invaluable assistance.",
-      image: globe,
+      image: f3,
     },
     {
       name: "Vikram Singh",
       content:
         "The journey of moving abroad seemed complex and daunting until I engaged with ZeeVista Advisors. Their proficient team simplified every step, providing clarity and confidence. I am now living my dream, and it's all thanks to their outstanding guidance.",
-      image: globe,
+      image: m4,
     },
     {
       name: "Naveen Kumar",
       content:
         "ZeeVista Advisors were instrumental in making my overseas education dream come true. Their seasoned professionals provided impeccable service and guidance at every step, ensuring a smooth visa application process. I am wholeheartedly grateful for their support.",
-      image: globe,
+      image: m5,
     },
     {
       name: "Mehak Ali",
       content:
         "The dedication and expertise of ZeeVista Advisors are truly commendable. They handled my case with utmost professionalism, making the daunting visa application process a straightforward experience. I highly recommend their services to anyone with immigration needs.",
-      image: globe,
+      image: f4,
     },
     {
       name: "Amir Sohail",
       content:
         "My experience with ZeeVista Advisors has been nothing short of excellent. Their proficient team guided me meticulously through every step, ensuring a successful visa application. Their dedication and expertise are why I am happily settled abroad today.",
-      image: globe,
+      image: m6,
     },
-    // New testimonials for specific services
     {
       name: "Zara Iqbal",
       content:
         "ZeeVista Advisors provided exceptional support in helping me obtain the UAE Golden Visa. Their team was highly knowledgeable about the application process and ensured all my documents were in order. Thanks to their guidance, I now have the long-term residency I dreamed of.",
-      image: globe,
+      image: f5,
     },
     {
       name: "Hassan Ali",
       content:
         "The PRO services provided by ZeeVista Advisors saved my business so much time. They handled everything from company registration to visa processing seamlessly. I highly recommend their professional and efficient services to any business in need of PRO support in Dubai.",
-      image: globe,
+      image: m7,
     },
     {
       name: "Sara Khan",
       content:
         "Thanks to ZeeVista Advisors' Banking Services, I was able to open my business account in the UAE quickly and efficiently. Their expertise and connections with local banks made the entire process hassle-free. I would definitely recommend their banking solutions to anyone setting up in the UAE.",
-      image: globe,
+      image: f6,
     },
     {
       name: "Manoj Desai",
       content:
         "ZeeVista Advisors helped me with my UAE Residence Visa, and I couldn’t be more satisfied. Their clear communication, professional approach, and timely updates made the entire process smooth. I’m now happily settled in Dubai thanks to their expert guidance.",
-      image: globe,
+      image: m8,
     },
     {
       name: "Fatima Al Zahra",
       content:
         "I engaged ZeeVista Advisors for Social Media Marketing Services for my startup, and they exceeded my expectations. Their creative campaigns boosted my online presence significantly. If you are looking to grow your brand through social media, ZeeVista is the go-to team.",
-      image: globe,
+      image: f7,
     },
     {
       name: "Imran Malik",
       content:
         "The website development team at ZeeVista Advisors did an incredible job creating my business website. It is modern, responsive, and perfectly aligned with my brand. They are thorough professionals who understand how to create a digital presence that delivers results.",
-      image: globe,
+      image: m9,
     },
     {
       name: "Ravi P",
       content:
         "I highly recommend ZeeVista's Portugal immigration services in Dubai; their consultants are top experts, and they offer comprehensive visa services.",
-      image: globe,
+      image: m10,
     },
   ];
 
@@ -126,7 +141,7 @@ export default function Testimonials() {
           life and business goals.
         </p>
         <div className="w-full flex items-center justify-center">
-          <div className="flex max-w-[500px] gap-10 py-10 md:flex-row items-center justify-center md:justify-between w-full">
+          <div className="flex max-w-[1000px] gap-10 py-10 md:flex-row items-center justify-center md:justify-between w-full">
             <div className="flex flex-col items-center">
               <VisibilitySensor
                 partialVisibility
@@ -135,11 +150,13 @@ export default function Testimonials() {
               >
                 {({ isVisible }: { isVisible: any }) => (
                   <h3 className="text-5xl font-bold text-yellow-600">
-                    <CountUp end={isVisible ? 10 : 0} duration={3} />
+                    <CountUp end={isVisible ? 500 : 0} duration={3} />+
                   </h3>
                 )}
               </VisibilitySensor>
-              <p className="text-center font-play text-lg mt-2 ">Countries</p>
+              <p className="text-center font-play text-lg mt-2 ">
+                Global Clients
+              </p>
             </div>
             <div className="flex flex-col items-center">
               <VisibilitySensor
@@ -149,12 +166,44 @@ export default function Testimonials() {
               >
                 {({ isVisible }: { isVisible: any }) => (
                   <h3 className="text-5xl font-bold text-yellow-600">
-                    <CountUp end={isVisible ? 467 : 0} duration={3} />
+                    <CountUp end={isVisible ? 10 : 0} duration={3} />+
                   </h3>
                 )}
               </VisibilitySensor>
               <p className="text-center text-lg mt-2 font-play">
-                Visa Approvals
+                Years of Experience
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <VisibilitySensor
+                partialVisibility
+                offset={{ top: 10 }}
+                delayedCall
+              >
+                {({ isVisible }: { isVisible: any }) => (
+                  <h3 className="text-5xl font-bold text-yellow-600">
+                    <CountUp end={isVisible ? 100 : 0} duration={3} />+
+                  </h3>
+                )}
+              </VisibilitySensor>
+              <p className="text-center text-lg mt-2 font-play">
+                Team of Professionals
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <VisibilitySensor
+                partialVisibility
+                offset={{ top: 10 }}
+                delayedCall
+              >
+                {({ isVisible }: { isVisible: any }) => (
+                  <h3 className="text-5xl font-bold text-yellow-600">
+                    <CountUp end={isVisible ? 15000 : 0} duration={3} />+
+                  </h3>
+                )}
+              </VisibilitySensor>
+              <p className="text-center text-lg mt-2 font-play">
+                Professional Guidelines
               </p>
             </div>
           </div>
